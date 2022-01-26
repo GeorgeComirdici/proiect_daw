03e5ff749fd36e09c10b46b60e9fcae922dd19b6
GEORGE ADRIAN COMIRDICI <george.comirdici@s.unibuc.ro>
2022-01-26 14:52:25 +0200

    adaugata clasa context

--- ProiectDAW/data/context.cs
+++ ProiectDAW/data/context.cs
@@ -1,7 +1,19 @@
-﻿namespace ProiectDAW.data
+﻿using ProiectDAW.Models;
+using Microsoft.EntityFrameworkCore;
+namespace ProiectDAW.data
 
 {
-    public class context
+    public class context : DbContext
     {
+        DbSet<angajati> Angajati { get; set; }
+        DbSet<Departamente> Departamente { get; set; }
+        DbSet<Proiecte> Proiecte { get; set; }
+        DbSet<AdreseAngajati> Adrese { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ProiectDAW");
+        }
     }
 }
